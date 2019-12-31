780a0f03ec2de31f80f9c7fa270c280c4621eb45
kayama <yuckey@sonicgarden.jp>
2019-12-31 12:49:20 +0900

    マテリアルデザイン

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        title: 'Flutter layout demo',
         home: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
-            ),
+          appBar: AppBar(
+            title: Text('Flutter layour demo'),
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            child: Icon(Icons.add),
-            tooltip: 'Increment',
+          body: Center(
+            child: Text('Hello World'),
           ),
         ),
       );
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 }
