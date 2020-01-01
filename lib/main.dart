77afb6e168745eaecac9df0917a279643b17650b
kayama <yuckey@sonicgarden.jp>
2020-01-01 14:49:52 +0900

    rowとcolumnによるレイアウト

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,113 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(color: Colors.white),
-        child: Center(
-          child: Text(
-            'Hello World',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 32, color: Colors.black87),
-          ),
-        ),
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Pavlova Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Pavlova(),
       );
+}
+
+class Pavlova extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    final ratings = Container(
+      padding: EdgeInsets.all(200),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    final iconList = DefaultTextStyle.merge(
+        style: descTextStyle,
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+          ]),
+        ));
+
+    final leftColum = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          Text('titleText'),
+          Text('subTitle'),
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    final sample = Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 440,
+          child: leftColum,
+        ),
+        Image.asset('images/main.jpeg')
+      ],
+    );
+
+    return Scaffold(
+      body: Center(
+        child: sample,
+      ),
+    );
+  }
 }
